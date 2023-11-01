cf05814e76b68656e489914c7771964267a479d7
EhsanMrf <developermrf@gmail.com>
2023-11-01 14:32:58 +0330

    ProductCommandHandler IMedaitReqest

---
+++ StoreProducts/StoreProducts.CoreService/Product/CommandHandler/ProductCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Common.Response;
+using MediatR;
+using StoreProducts.Core.Product.Command;
+using StoreProducts.Core.Product.RepositoryCommand;
+
+namespace StoreProducts.CoreService.Product.CommandHandler;
+
+public class ProductCommandHandler:IRequestHandler<ProductCommand,ServiceResponse<Core.Product.Entity.Product>>
+{
+    private readonly IProductRepository _repository;
+
+    public ProductCommandHandler(IProductRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<ServiceResponse<Core.Product.Entity.Product>> Handle(ProductCommand request, CancellationToken cancellationToken)
+    {
+        return await _repository.Create(request);
+    }
+}
