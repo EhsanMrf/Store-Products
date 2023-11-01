09a8647e8cbb88fd529459b57d1ed716581c30d1
EhsanMrf <developermrf@gmail.com>
2023-11-01 14:29:42 +0330

    define abstraction IProductRepository

---
+++ StoreProducts/StoreProducts.Core/Product/RepositoryCommand/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Common.Response;
+using Common.TransientService;
+using StoreProducts.Core.Product.Command;
+
+namespace StoreProducts.Core.Product.RepositoryCommand;
+
+public interface IProductRepository : ITransientService
+{
+    Task<ServiceResponse<Entity.Product>> Create(ProductCommand  command);
+}
