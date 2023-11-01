720f7152b9486e6684cdb16273be3c48a24f4284
EhsanMrf <developermrf@gmail.com>
2023-11-01 11:21:58 +0330

    Infrastructure Result and return client
    Definr Service Response  DataList from Generic <T>

---
+++ StoreProducts/Common/Input/BaseListInput.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Int32;
+namespace Common.Input
+{
+    public class BaseListInput
+    {
+        [Range(1, MaxValue)] public virtual int? Page { get; set; } = 1;
+        [Range(1, MaxValue)] public virtual int PageSize { get; set; } = 9999;
+    }
+}
---
+++ StoreProducts/Common/Response/ServiceResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Response;
+
+public class ServiceResponse
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+}
+public class ServiceResponse<T> : ServiceResponse
+{
+    public T Data { get; set; }
+}
