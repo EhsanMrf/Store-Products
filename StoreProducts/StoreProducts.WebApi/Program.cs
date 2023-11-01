8a654589802fecfc0bd1f02de00b050501d30a0e
EhsanMrf <developermrf@gmail.com>
2023-11-01 15:45:04 +0330

    ServiceExtensions in program.cs

--- StoreProducts/StoreProducts.WebApi/Program.cs
+++ StoreProducts/StoreProducts.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using StoreProducts.WebApi.ProviderExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.DatabaseContext(builder.Configuration.GetSection("Connection").Value);
+builder.Services.InternalizeDataBase();
+builder.Services.InjectScope();
 
 var app = builder.Build();
 
--- StoreProducts/StoreProducts.WebApi/ProviderExtensions/ServiceProviderServiceExtensions.cs
+++ StoreProducts/StoreProducts.WebApi/ProviderExtensions/ServiceProviderServiceExtensions.cs
@@ -1,14 +1,38 @@
 ﻿using Common.TransientService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StoreProducts.Core.User.Entity;
+using StoreProducts.Infrastructure.Database;
+using StoreProducts.Infrastructure.Date.Product.Command;
+using StoreProducts.Infrastructure.Product.Builder;
 
 namespace StoreProducts.WebApi.ProviderExtensions;
 
 public static class ServiceProviderServiceExtensions
 {
-    public static void GetService<T>(this IServiceCollection services)
+    public static void InjectScope(this IServiceCollection services)
     {
-        services.Scan(scan => scan.FromAssembliesOf(typeof(RegisterRepository))
+        services.Scan(scan => scan.FromAssembliesOf(typeof(ProductRepository))
             .AddClasses(classes => classes.AssignableTo<ITransientService>())
             .AsImplementedInterfaces()
             .WithScopedLifetime());
+
+        services.Scan(scan => scan.FromAssembliesOf(typeof(ProductBuilder))
+            .AddClasses(classes => classes.AssignableTo<ITransientServiceInfrastructure>())
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+    }  
+    
+    public static void InternalizeDataBase(this IServiceCollection services)
+    {
+        services
+            .AddIdentity<User, IdentityRole<int>>()
+            .AddEntityFrameworkStores<DatabaseContext>()
+            .AddDefaultTokenProviders();
+    }
+    
+    public static void DatabaseContext(this IServiceCollection services, string connection)
+    {
+        services.AddDbContext<DatabaseContext>(x => x.UseSqlServer(connection));
     }
 }
