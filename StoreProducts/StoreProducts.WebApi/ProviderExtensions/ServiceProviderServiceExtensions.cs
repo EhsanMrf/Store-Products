f94bd483eeedfdd698a63e89799badbcdf373dbd
EhsanMrf <developermrf@gmail.com>
2023-11-01 15:23:25 +0330

    install package Scrutor and refrence

---
+++ StoreProducts/StoreProducts.WebApi/ProviderExtensions/ServiceProviderServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Common.TransientService;
+
+namespace StoreProducts.WebApi.ProviderExtensions;
+
+public static class ServiceProviderServiceExtensions
+{
+    public static void GetService<T>(this IServiceCollection services)
+    {
+        services.Scan(scan => scan.FromAssembliesOf(typeof(RegisterRepository))
+            .AddClasses(classes => classes.AssignableTo<ITransientService>())
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+    }
+}
