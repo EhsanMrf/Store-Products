c3c6e1d8006a9e2fe801273010d10e383722ff63
EhsanMrf <developermrf@gmail.com>
2023-11-01 22:05:20 +0330

    UpdateProduct IRequest

--- StoreProducts/StoreProducts.Core/Product/Command/UpdateProductCommand.cs
+++ StoreProducts/StoreProducts.Core/Product/Command/UpdateProductCommand.cs
@@ -3,11 +3,11 @@ using MediatR;
 
 namespace StoreProducts.Core.Product.Command;
 
-public class CreateProductCommand :IRequest<ServiceResponse<Entity.Product>>
+public class UpdateProductCommand : IRequest<ServiceResponse<bool>>
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public string ManufacturePhone { get; set; }
     public string ManufactureEmail { get; set; }
     public bool IsAvailable { get; set; }
-    public DateTime ProduceDate { get; set; }
 }
--- StoreProducts/StoreProducts.Core/Product/RepositoryCommand/IProductRepository.cs
+++ StoreProducts/StoreProducts.Core/Product/RepositoryCommand/IProductRepository.cs
@@ -7,5 +7,5 @@ namespace StoreProducts.Core.Product.RepositoryCommand;
 public interface IProductRepository : ITransientService
 {
     Task<ServiceResponse<Entity.Product>> Create(CreateProductCommand  command);
-    Task<ServiceResponse<bool>> Update();
+    Task<ServiceResponse<bool>> Update(UpdateProductCommand command);
 }
