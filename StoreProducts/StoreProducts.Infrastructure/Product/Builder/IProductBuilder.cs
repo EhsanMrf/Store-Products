6b611bb21ea813ad7ffe998b779cc0bdf44aa411
EhsanMrf <developermrf@gmail.com>
2023-11-01 15:13:21 +0330

    builder pattern

---
+++ StoreProducts/StoreProducts.Infrastructure/Product/Builder/IProductBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreProducts.Infrastructure.Product.Builder;
+
+public interface IProductBuilder
+{
+    IProductBuilder WithName(string name);
+    IProductBuilder WithManufacturePhone(string manufacturePhone);
+    IProductBuilder WithManufactureEmail(string manufactureEmail);
+    IProductBuilder WithAvailable(bool available);
+    IProductBuilder WithProduceDate(DateTime produceDate);
+    Core.Product.Entity.Product Build();
+}
