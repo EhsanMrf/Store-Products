4fbec3eccec9db20bf922b07635ac846ad1b3721
EhsanMrf <developermrf@gmail.com>
2023-11-01 13:33:54 +0330

    update by id with Mapping

--- StoreProducts/Common/OperationCrud/CrudManager.cs
+++ StoreProducts/Common/OperationCrud/CrudManager.cs
@@ -1,11 +1,19 @@
-﻿using Common.Entity;
+﻿using AutoMapper;
+using Common.Entity;
 using Common.Response;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography;
+using System.Security.Principal;
+
 namespace Common.OperationCrud;
 
 public class CrudManager<T, TId> : ICrudManager<T, TId> where T : BaseEntity<TId> where TId : struct, IComparable
 {
+    private readonly IMapper _mapper;
+
+    public CrudManager(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
 
     public async Task<bool> Insert(DbContext dbContext, T entity)
     {
@@ -24,17 +32,32 @@ public class CrudManager<T, TId> : ICrudManager<T, TId> where T : BaseEntity<TId
         return entity;
     }
 
-    public async Task<ServiceResponse<T>> GetById(DbContext dbContext, ServiceResponse<T> entity, TId id)
+    public async Task<ServiceResponse<T>> UpdateById(DbContext dbContext, ServiceResponse<T> serviceEntity, TId id,T inputEntity)
+    {
+        var oldEntity = await GetById(dbContext, serviceEntity, id);
+        var entity = _mapper.Map(inputEntity, oldEntity.Data);
+        dbContext.Entry(entity).State = EntityState.Modified;
+        var save = await dbContext.SaveChangesAsync() > 0;
+        if (!save) return null;
+        
+        serviceEntity.Data = entity;
+        return serviceEntity;
+    }
+
+    public async Task<ServiceResponse<T>> GetById(DbContext dbContext, ServiceResponse<T> serviceEntity, TId id)
     {
         var data = await dbContext.Set<T>().Where(q => q.Id.Equals(id)).FirstOrDefaultAsync();
-        entity.Data = data;
-        return entity.Data is not null ? entity : new ServiceResponse<T> { Data = null, Message = "No Data" };
+        serviceEntity.Data = data;
+        return serviceEntity.Data is not null ? serviceEntity : new ServiceResponse<T> { Data = null, Message = "No Data" };
     }
+
+
 }
 
 public interface ICrudManager<T, TId> where T : BaseEntity<TId> where TId : struct, IComparable
 {
     Task<bool> Insert(DbContext dbContext, T entity);
-    Task<ServiceResponse<T>> Insert(DbContext dbContext, ServiceResponse<T> entity);
-    Task<ServiceResponse<T>> GetById(DbContext dbContext, ServiceResponse<T> entity, TId id);
+    Task<ServiceResponse<T>> Insert(DbContext dbContext, ServiceResponse<T> serviceEntity);
+    Task<ServiceResponse<T>> UpdateById(DbContext dbContext, ServiceResponse<T> serviceEntity, TId id, T inputEntity);
+    Task<ServiceResponse<T>> GetById(DbContext dbContext, ServiceResponse<T> serviceEntity, TId id);
 }
