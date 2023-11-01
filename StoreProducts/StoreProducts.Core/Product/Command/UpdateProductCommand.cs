23930aa2d08c4693b766f7ea0f171a63fbbfdf41
EhsanMrf <developermrf@gmail.com>
2023-11-01 22:37:06 +0330

    rebuild

--- StoreProducts/StoreProducts.Core/Product/Command/UpdateProductCommand.cs
+++ StoreProducts/StoreProducts.Core/Product/Command/UpdateProductCommand.cs
@@ -3,6 +3,10 @@ using MediatR;
 
 namespace StoreProducts.Core.Product.Command;
 
-public class UpdateProductCommand : Entity.Product, IRequest<ServiceResponse<bool>>
+public class UpdateProductCommand :IRequest<ServiceResponse<bool>>
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ManufacturePhone { get; set; }
+    public string ManufactureEmail { get; set; }
 }
