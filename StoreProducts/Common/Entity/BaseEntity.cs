71e41106b223c5783c94e5c643fc5b026149f9a7
EhsanMrf <developermrf@gmail.com>
2023-11-01 11:16:48 +0330

    mkdir Entity create Class BaseEntity and EntityDef for extend automatic

---
+++ StoreProducts/Common/Entity/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Entity;
+
+public class BaseEntity<T> : EntityDef
+{
+    public T Id { get; set; }
+    public DateTime CreateDateTime { get; set; }
+    public DateTime? UpdateDateTime { get; set; }
+    public bool IsDeleted { get; set; }
+}
