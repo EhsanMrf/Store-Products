4818c87a94a081d510e6014a15966634bf7cc2b3
EhsanMrf <developermrf@gmail.com>
2023-11-01 14:24:19 +0330

    add prop Product Entity

--- StoreProducts/StoreProducts.Core/Product/Entity/Product.cs
+++ StoreProducts/StoreProducts.Core/Product/Entity/Product.cs
@@ -4,5 +4,9 @@ namespace StoreProducts.Core.Product.Entity;
 
 public class Product :BaseEntity<int>
 {
-
+    public string Name { get; set; }
+    public string ManufacturePhone { get; set; }
+    public string ManufactureEmail { get; set; }
+    public bool IsAvailable { get; set; }
+    public DateTime ProduceDate { get; set; }
 }
