d6c0a7925c83469c658f491d5615ec0bb0c0458f
EhsanMrf <developermrf@gmail.com>
2023-11-03 02:05:44 +0330

    define IRegisterRepository create Command

---
+++ StoreProducts/StoreProducts.Core/User/Command/UserRegisterCommand.cs
@@ -0,0 +1,12 @@
+﻿using Common.Response;
+using MediatR;
+
+namespace StoreProducts.Core.User.Command;
+
+public class UserRegisterCommand :IRequest<ServiceResponse<bool>>
+{
+    public string FullName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string ConfirmPassword { get; set; }
+}
---
+++ StoreProducts/StoreProducts.Core/User/RepositoryCommand/IRegisterRepository.cs
@@ -0,0 +1,9 @@
+﻿using Common.TransientService;
+using StoreProducts.Core.User.Command;
+
+namespace StoreProducts.Core.User.RepositoryCommand;
+
+public  interface IRegisterRepository : ITransientService
+{
+    Task<bool> UserRegister(UserRegisterCommand  command);
+}
