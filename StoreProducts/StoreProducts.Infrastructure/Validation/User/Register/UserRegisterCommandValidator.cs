f67bdb9fea68553db3a453248e1628d473d4d21d
EhsanMrf <developermrf@gmail.com>
2023-11-03 02:17:46 +0330

    validation user

---
+++ StoreProducts/StoreProducts.Infrastructure/Validation/User/Register/UserRegisterCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using StoreProducts.Core.User.Command;
+
+namespace StoreProducts.Infrastructure.Validation.User.Register;
+
+public class UserRegisterCommandValidator :AbstractValidator<UserRegisterCommand>
+{
+    public UserRegisterCommandValidator()
+    {
+        RuleFor(x => x.FullName).MinimumLength(4);
+        RuleFor(x => x.UserName).NotEmpty();
+        RuleFor(x => x.UserName).MinimumLength(4);
+        
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password);
+    }
+}
