f71e8d591a2e3d308535aef7e58f12c4da92c38a
EhsanMrf <developermrf@gmail.com>
2023-11-03 00:54:37 +0330

    cute class

---
+++ StoreProducts/StoreProducts.Infrastructure/Validation/Product/CreateProductCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using StoreProducts.Core.Product.Command;
+
+namespace StoreProducts.Infrastructure.Validation.Product;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Name).MinimumLength(3);
+        RuleFor(x => x.ManufactureEmail).EmailAddress();
+
+        RuleFor(x => x.ProduceDate).
+            Must(date => date != default)
+            .WithMessage("ProduceDate is required"); ;
+
+    }
+
+}
