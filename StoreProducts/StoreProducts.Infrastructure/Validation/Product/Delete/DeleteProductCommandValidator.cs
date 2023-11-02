2ef1642f63b6bc9f1b8bd9e9ef5a8efd75460024
EhsanMrf <developermrf@gmail.com>
2023-11-03 01:08:13 +0330

    update and delete ProducValidator

---
+++ StoreProducts/StoreProducts.Infrastructure/Validation/Product/Delete/DeleteProductCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using StoreProducts.Core.Product.Command;
+
+namespace StoreProducts.Infrastructure.Validation.Product.Delete;
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id).Must(q=>q>0)
+            .WithMessage("Please enter a valid Id");
+    }
+}
---
+++ StoreProducts/StoreProducts.Infrastructure/Validation/Product/Update/UpdateProductCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using StoreProducts.Core.Product.Command;
+
+namespace StoreProducts.Infrastructure.Validation.Product.Update;
+
+public class UpdateProductCommandValidator :AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).Must(q => q > 0)
+            .WithMessage("Please enter a valid Id");
+
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Name).MinimumLength(3);
+        RuleFor(x => x.ManufactureEmail).EmailAddress();
+    }
+}
