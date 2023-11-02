b1023b49886a8f2b771e3959bda2f9ab184f09c0
EhsanMrf <developermrf@gmail.com>
2023-11-03 02:34:33 +0330

    added RuleFor

--- StoreProducts/StoreProducts.Infrastructure/Validation/User/Register/UserRegisterCommandValidator.cs
+++ StoreProducts/StoreProducts.Infrastructure/Validation/User/Register/UserRegisterCommandValidator.cs
@@ -7,9 +7,14 @@ public class UserRegisterCommandValidator :AbstractValidator<UserRegisterCommand
 {
     public UserRegisterCommandValidator()
     {
+        RuleFor(x => x.FullName).NotEmpty();
         RuleFor(x => x.FullName).MinimumLength(4);
+
         RuleFor(x => x.UserName).NotEmpty();
         RuleFor(x => x.UserName).MinimumLength(4);
+
+        RuleFor(x => x.Password).NotEmpty();
+        RuleFor(x => x.Password).MinimumLength(5);
         
         RuleFor(x => x.ConfirmPassword)
             .Equal(x => x.Password);
