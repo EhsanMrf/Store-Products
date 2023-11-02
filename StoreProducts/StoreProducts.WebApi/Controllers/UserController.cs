0d7317f33b60fde5caf57a7f667991abd2ec5aff
EhsanMrf <developermrf@gmail.com>
2023-11-03 02:30:54 +0330

    userContrroller create and api

---
+++ StoreProducts/StoreProducts.WebApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Common.Response;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StoreProducts.Core.User.Command;
+
+namespace StoreProducts.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator) => _mediator = mediator;
+
+        [HttpPost("RegisterUser")]
+        public Task<ServiceResponse<bool>> RegisterUser([FromQuery]UserRegisterCommand input)
+        {
+            return _mediator.Send(input);
+        }
+    }
+}
