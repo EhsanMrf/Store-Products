afc2d7ca67278bfc64dd38cae2025d7e77b459a7
EhsanMrf <developermrf@gmail.com>
2023-11-03 02:23:27 +0330

    implement user register

---
+++ StoreProducts/StoreProducts.Infrastructure.Date/User/Command/UserRegisterRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using StoreProducts.Core.User.Command;
+using StoreProducts.Core.User.RepositoryCommand;
+using StoreProducts.Infrastructure.User.Builder;
+
+namespace StoreProducts.Infrastructure.Date.User.Command;
+
+public class UserRegisterRepository :IUserRegisterRepository
+{
+    private readonly UserManager<Core.User.Entity.User> _userManager;
+    private readonly IUserBuilder _userBuilder;
+
+    public UserRegisterRepository(UserManager<Core.User.Entity.User> userManager, IUserBuilder userBuilder)
+    {
+        _userManager = userManager;
+        _userBuilder = userBuilder;
+    }
+
+    public async Task<bool> UserRegister(UserRegisterCommand command)
+    {
+        var user = _userBuilder.WithUserName(command.UserName).WithFullName(command.FullName).Build();
+        var identityResult = await _userManager.CreateAsync(user, command.Password);
+        return identityResult.Succeeded;
+    }
+}
