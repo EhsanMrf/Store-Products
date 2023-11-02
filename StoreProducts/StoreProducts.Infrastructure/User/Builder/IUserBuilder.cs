acac81078f2211e722d7bdc639e9a7c330824c20
EhsanMrf <developermrf@gmail.com>
2023-11-03 02:21:47 +0330

    define patter builder user

---
+++ StoreProducts/StoreProducts.Infrastructure/User/Builder/IUserBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Common.TransientService;
+
+namespace StoreProducts.Infrastructure.User.Builder;
+
+public interface IUserBuilder : ITransientServiceInfrastructure
+{
+    IUserBuilder WithFullName(string fullName);
+    IUserBuilder WithUserName(string userName);
+    IUserBuilder WithEmail(string email);
+
+    Core.User.Entity.User Build();
+}
---
+++ StoreProducts/StoreProducts.Infrastructure/User/Builder/UserBuilder.cs
@@ -0,0 +1,37 @@
+﻿namespace StoreProducts.Infrastructure.User.Builder
+{
+    public class UserBuilder : IUserBuilder
+    {
+        private string FullName { get; set; }
+        private string UserName { get; set; }
+        private string Email { get; set; }
+
+        public IUserBuilder WithFullName(string fullName)
+        {
+            FullName = fullName;
+            return this;
+        }
+
+        public IUserBuilder WithUserName(string userName)
+        {
+            UserName = userName;
+            return this;
+        }
+
+        public IUserBuilder WithEmail(string email)
+        {
+            Email = email;
+            return this;
+        }
+
+        public Core.User.Entity.User Build()
+        {
+            return new Core.User.Entity.User
+            {
+                FullName = FullName,
+                UserName = UserName,
+                Email = UserName,
+            };
+        }
+    }
+}
