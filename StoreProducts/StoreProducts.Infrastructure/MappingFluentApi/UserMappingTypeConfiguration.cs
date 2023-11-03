448c3aeb6a75d5c2e3eda49b6a3aec8e3a146687
EhsanMrf <developermrf@gmail.com>
2023-11-04 01:07:09 +0330

    sparce performance page and data size

--- StoreProducts/StoreProducts.Infrastructure/MappingFluentApi/UserMappingTypeConfiguration.cs
+++ StoreProducts/StoreProducts.Infrastructure/MappingFluentApi/UserMappingTypeConfiguration.cs
@@ -10,5 +10,9 @@ public class UserMappingTypeConfiguration : IEntityTypeConfiguration<Core.User.E
         builder.Property(x => x.UserName).HasMaxLength(150);
         builder.Property(x => x.Email).HasMaxLength(40);
         builder.Property(x => x.FullName).HasMaxLength(150);
+        builder.Property(x => x.PhoneNumber).HasMaxLength(25);
+
+        //sparse for data null => performance page and data size -- 
+        builder.Property(x => x.PhoneNumber).IsSparse();
     }
 }
