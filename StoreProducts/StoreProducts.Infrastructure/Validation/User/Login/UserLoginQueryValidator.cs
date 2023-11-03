e2858090e8d4687c09d5acce684ece9eec7c9ea2
EhsanMrf <developermrf@gmail.com>
2023-11-03 13:41:33 +0330

    UserLogin validation

---
+++ StoreProducts/StoreProducts.Infrastructure/Validation/User/Login/UserLoginQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using StoreProducts.Core.User.Query;
+
+namespace StoreProducts.Infrastructure.Validation.User.Login
+{
+    public class UserLoginQueryValidator:AbstractValidator<UserLoginQuery>
+    {
+        public UserLoginQueryValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).NotEmpty().NotNull();
+        }
+    }
+}
