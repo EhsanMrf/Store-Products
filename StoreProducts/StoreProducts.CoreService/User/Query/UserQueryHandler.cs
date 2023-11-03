e54b49afa837026a0cf6e51e5ed45db6c9e8d4ff
EhsanMrf <developermrf@gmail.com>
2023-11-03 13:41:05 +0330

    QueryHandler

---
+++ StoreProducts/StoreProducts.CoreService/User/Query/UserQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Common.Response;
+using MediatR;
+using StoreProducts.Core.User.Query;
+using StoreProducts.Core.User.RepositoryQuery;
+
+namespace StoreProducts.CoreService.User.Query;
+
+public class UserQueryHandler :IRequestHandler<UserLoginQuery,ServiceResponse<bool>>
+{
+    private readonly IUserQueryRepository _userQueryRepository;
+
+    public UserQueryHandler(IUserQueryRepository userQueryRepository)
+    {
+        _userQueryRepository = userQueryRepository;
+    }
+
+    public async Task<ServiceResponse<bool>> Handle(UserLoginQuery request, CancellationToken cancellationToken)
+    {
+        return await _userQueryRepository.Login(request);
+    }
+}
