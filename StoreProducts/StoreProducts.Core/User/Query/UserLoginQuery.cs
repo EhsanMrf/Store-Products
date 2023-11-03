f6551b6af26129cec66cbe663591c0817ecbc906
EhsanMrf <developermrf@gmail.com>
2023-11-03 13:28:11 +0330

    user Query

---
+++ StoreProducts/StoreProducts.Core/User/Query/UserLoginQuery.cs
@@ -0,0 +1,10 @@
+﻿using Common.Response;
+using MediatR;
+
+namespace StoreProducts.Core.User.Query;
+
+public class UserLoginQuery :IRequest<ServiceResponse<bool>>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
