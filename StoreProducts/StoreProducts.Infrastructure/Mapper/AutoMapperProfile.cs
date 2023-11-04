847f1cffe4881ba93be335a7c5d6973b87ef0349
EhsanMrf <developermrf@gmail.com>
2023-11-04 19:55:28 +0330

    mapp

--- StoreProducts/StoreProducts.Infrastructure/Mapper/AutoMapperProfile.cs
+++ StoreProducts/StoreProducts.Infrastructure/Mapper/AutoMapperProfile.cs
@@ -1,14 +1,15 @@
 ﻿using AutoMapper;
 using StoreProducts.Core.Product.Command;
+using StoreProducts.Core.Product.Query.Result;
 
 namespace StoreProducts.Infrastructure.Mapper
 {
-    public class AutoMapperProfile :Profile
+    public class AutoMapperProfile : Profile
     {
         public AutoMapperProfile()
         {
-        CreateMap<Core.Product.Entity.Product,UpdateProductCommand>().ReverseMap();
-
+            CreateMap<Core.Product.Entity.Product, UpdateProductCommand>().ReverseMap();
+            CreateMap<Core.Product.Entity.Product, ProductResult>().ReverseMap();
         }
     }
 }
