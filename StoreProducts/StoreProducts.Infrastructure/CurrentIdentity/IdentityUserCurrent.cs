834538c881671c0818138d7287ed811d82c543d2
EhsanMrf <developermrf@gmail.com>
2023-11-04 15:58:53 +0330

    abstract class authoriz

---
+++ StoreProducts/StoreProducts.Infrastructure/CurrentIdentity/IdentityUserCurrent.cs
@@ -0,0 +1,28 @@
+using System.Linq.Expressions;
+using Common.BaseService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreProducts.Infrastructure.CurrentIdentity;
+
+public abstract class IdentityUserCurrent :BaseService
+{
+    private readonly IHttpContextAccessor _accessor;
+    private readonly UserManager<Core.User.Entity.User> _userManager;
+    protected IdentityUserCurrent(IHttpContextAccessor accessor, UserManager<Core.User.Entity.User> userManager)
+    {
+        _accessor = accessor;
+        _userManager = userManager;
+    }
+
+    public async Task<Core.User.Entity.User> CurrentUser()
+    {
+        var identityUser = _accessor.HttpContext!.User.Claims.FirstOrDefault(x => x.Type.Equals("IdentityUser")).Value;
+        var userBySelect = GetUserBySelect();
+        var firstOrDefaultAsync = await _userManager.Users.Where(x=>x.Identity.Equals(identityUser)).Select(userBySelect).FirstOrDefaultAsync();
+        return firstOrDefaultAsync;
+    }
+
+    public abstract Expression<Func<Core.User.Entity.User, Core.User.Entity.User>> GetUserBySelect();
+}
